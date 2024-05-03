f31dbf28d7520202035521318007392938fbc714
Monseymour <fabliniosales@gmail.com>
2024-05-03 00:26:18 -0300

    Correção do codigo (icone)

--- design/cartazes.js
+++ design/cartazes.js
@@ -40,7 +40,7 @@ function renderCartaz() {
 		template.innerHTML= `
 		<img class="cartaz" src="./assets/images/cartaz-1.jpg" alt="Cartaz de Procurado">
 		<section class="cartazDetalhe">
-			<div><h3><strong>Nome</strong> </h3><img src="../assets/icones/lutador.png" alt="Icone da Organização"></div>
+			<div><h3><strong>Nome</strong> </h3><img src="./assets/icones/lutador.png" alt="Icone da Organização"></div>
 			<h2>${renderizar.nome}</h2>
 			<p><strong class="textoSombreado">Organização</strong> <br> ${renderizar.organizacao} </p>
 			<p><strong class="textoSombreado">Crime</strong> <br> ${renderizar.crime} </p>
